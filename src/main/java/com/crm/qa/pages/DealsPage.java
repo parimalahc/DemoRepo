9e537658ad767fc0c51278e433ebad03553d15c9
parimalahc <parimalapawankumar@gmail.com>
2019-06-28 01:31:33 +0530

    Dealspage

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -1,8 +1,14 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class DealsPage {
 	
+	
+	WebDriver driver;
 	public DealsPage() {
+		
+		this.driver = driver;
 		
 	}
 
